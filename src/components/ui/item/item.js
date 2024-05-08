ef97b324ee7adc7cdf36c0a1e42ad36a33968240
Andrey Semenov <semenov.orc@gmail.com>
2024-05-08 16:53:47 +0300

    add comp size file

--- src/components/ui/item/item.js
+++ src/components/ui/item/item.js
@@ -1,10 +1,17 @@
 import getElement from '@/js/getElement'
 import styles from './item.module.css'
 
+/**
+ * Creates an item element based on the file information.
+ *
+ * @param {Object} file - The file object containing information like url and name.
+ * @return {HTMLElement} The created item element.
+ */
 export const item = (file) => {
+  const fileSize = getSize(file.url)
   const item = getElement({ tag: 'div', classes: styles.item })
   const name = getElement({ tag: 'div', classes: styles.name, textContent: file.name })
-  const size = getElement({ tag: 'div', classes: styles.size, textContent: file.size })
+  const size = getElement({ tag: 'div', classes: styles.size, textContent: fileSize })
   const link = getElement({
     tag: 'a',
     classes: styles.link,
@@ -12,11 +19,24 @@ export const item = (file) => {
     href: file.url,
     download: file.name,
     rel: 'noopener',
+    data: { size: fileSize },
   })
 
   item.append(name, size, link)
 
   return item
+}
+
+/**
+ * Calculates the size of a base64 string in megabytes.
+ *
+ * @param {string} str64 - The base64 string to calculate the size from.
+ * @return {string} The size of the base64 string formatted in MB.
+ */
+const getSize = (str64) => {
+  const size = window.atob(str64.split(',')[1]).length / 1024 / 1024
+
+  return `${Math.round(size * 100) / 100} MB`
 }
 
 export default item
