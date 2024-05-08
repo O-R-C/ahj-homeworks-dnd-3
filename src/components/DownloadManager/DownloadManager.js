3f6172dc2c57d70706e709d2342cd771becd4f94
Andrey Semenov <semenov.orc@gmail.com>
2024-05-08 18:39:08 +0300

    add description

--- src/components/DownloadManager/DownloadManager.js
+++ src/components/DownloadManager/DownloadManager.js
@@ -1,11 +1,22 @@
 import DownloadManagerUI from './DownloadManagerUI'
 
+/**
+ * Class representing a Download Manager that listens for click events and fires startDownload events.
+ * @event click - Listens for click events on the app element.
+ * @listens DownloadManager#click
+ * @fires DownloadManager#startDownload
+ */
 export default class DownloadManager {
   #ui = new DownloadManagerUI()
   #app = this.#ui.app
   #data
   #element
 
+  /**
+   * Create a Download Manager.
+   * @param {string} element - The HTML element to bind the Download Manager to.
+   * @param {Array} data - The array of data representing the download items.
+   */
   constructor(element, data) {
     this.#data = data
     this.#element = this.#ui.getElement(element)
@@ -13,26 +24,43 @@ export default class DownloadManager {
     this.#init()
   }
 
+  /**
+   * Initializes the Download Manager.
+   */
   #init() {
     this.#bindToDom()
     this.#addElements()
     this.#addEventListener()
   }
 
+  /**
+   * Binds the Download Manager to the DOM.
+   */
   #bindToDom() {
     this.#element.append(this.#app)
   }
 
+  /**
+   * Adds the download items to the Download Manager.
+   */
   #addElements() {
     this.#data.forEach((file) => {
       this.#ui.addItem(file)
     })
   }
 
+  /**
+   * Adds event listeners.
+   */
   #addEventListener() {
     this.#app.addEventListener('click', this.#onClick)
   }
 
+  /**
+   * Handles the click event.
+   * @fires DownloadManager#startDownload
+   * @param {MouseEvent} e - The click event.
+   */
   #onClick = (e) => {
     const link = e.target.closest('a')
 
@@ -41,6 +69,11 @@ export default class DownloadManager {
     this.#fireStartDownloadEvent(link.dataset.size)
   }
 
+  /**
+   * Get the startDownload event with the specified size.
+   * @param {string} size - The size of the download.
+   * @returns {CustomEvent} The startDownload event.
+   */
   #getStartDownloadEvent(size) {
     return new CustomEvent('startDownload', {
       detail: {
@@ -49,6 +82,10 @@ export default class DownloadManager {
     })
   }
 
+  /**
+   * Fire the startDownload event with the specified size.
+   * @param {string} size - The size of the download to start.
+   */
   #fireStartDownloadEvent(size) {
     document.dispatchEvent(this.#getStartDownloadEvent(size))
   }
--- src/components/DownloadManager/DownloadManagerUI.js
+++ src/components/DownloadManager/DownloadManagerUI.js
@@ -3,9 +3,18 @@ import item from '@/components/ui/item/item'
 import DownloadStatistics from '@/components/DownloadStatistics/DownloadStatistics'
 import styles from './DownloadManager.module.css'
 
+/**
+ * Class representing the UI for Download Manager.
+ */
 export default class DownloadManagerUI {
   #containerFiles
 
+  /**
+   * Get the HTML element by tag or selector.
+   *
+   * @param {string | HTMLElement} element - Element tag or selector.
+   * @return {HTMLElement} The selected element.
+   */
   getElement(element) {
     if (typeof element === 'string') {
       element = document.querySelector(element)
@@ -13,6 +22,11 @@ export default class DownloadManagerUI {
     return element
   }
 
+  /**
+   * Get the app element for the Download Manager UI.
+   *
+   * @returns {HTMLElement} The app element.
+   */
   get app() {
     const app = getElement({ tag: 'div', classes: styles.app })
     const wrapper = getElement({ tag: 'div', classes: styles.wrapper })
@@ -32,10 +46,20 @@ export default class DownloadManagerUI {
     return app
   }
 
+  /**
+   * Adds an item to the containerFiles element.
+   *
+   * @param {Object} file - The file object to add.
+   */
   addItem(file) {
     this.#containerFiles.append(item(file))
   }
 
+  /**
+   * Adds the download statistics to the app element.
+   *
+   * @param {HTMLElement} element - The element to add the download statistics to.
+   */
   #addStatistics(element) {
     new DownloadStatistics(element)
   }
